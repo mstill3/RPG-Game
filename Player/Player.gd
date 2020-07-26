6c84116382a6f9191a9e746f51fd69f68242b627
Matt Stillwell <stillwell006@gmail.com>
2020-07-26 14:48:32 -0400

    Up to lesson 6, adds player animations for running, and collision boxes from bushes

---
+++ Player/Player.gd
@@ -0,0 +1,58 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+const ACCELERATION = 200
+const MAX_SPEED = 80
+const FRICTION = 250
+
+var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+var animation = null
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+	#pass # Replace with function body.
+	#print("hello world")
+
+#func moveX():
+#	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+#		velocity.x = 0
+#	elif Input.is_action_pressed("ui_right"):
+#		velocity.x = speed
+#	elif Input.is_action_pressed("ui_left"):
+#		velocity.x = -speed
+#	else: 
+#		velocity.x = 0
+
+#func _ready():
+#	# $ means part of same scene
+#	animationPlayer = $AnimationPlayer
+
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animation = "RunRight"
+		else:
+			animation = "RunLeft"
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animation = "IdleRight"
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	animationPlayer.play(animation)
+	velocity = move_and_slide(velocity)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
