c74905020ca98f48afe4b0f0257e38f320f34757
Matt Stillwell <stillwell006@gmail.com>
2020-08-01 14:20:34 -0400

    Grass, attack, roll

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,67 +1,76 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 const ACCELERATION = 200
 const MAX_SPEED = 90
 const FRICTION = 350
 
+enum {
+	MOVE,
+	ATTACK,
+	ROLL
+}
+
 var velocity = Vector2.ZERO
+var animation: String = "IdleRight"
+var state = MOVE
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
-var animation: String = "IdleRight"
-
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-	#pass # Replace with function body.
-	#print("hello world")
-
-#func moveX():
-#	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
-#		velocity.x = 0
-#	elif Input.is_action_pressed("ui_right"):
-#		velocity.x = speed
-#	elif Input.is_action_pressed("ui_left"):
-#		velocity.x = -speed
-#	else: 
-#		velocity.x = 0
-
-#func _ready():
-#	# $ means part of same scene
-#	animationPlayer = $AnimationPlayer
 
 
 func _physics_process(delta):
+	match(state):
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			attack_state(delta)
+		ROLL:
+			roll_state(delta)
+	
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		#if input_vector.x > 0:
-		#	animation = "RunRight"
-		#else:
-		#	animation = "RunLeft"
-		
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		
 		animationState.travel("Run")
-		
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		#animation = "IdleRight"
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	# animationPlayer.play(animation)
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+	elif Input.is_action_just_pressed("roll"):
+		state = ROLL
+	
+	
+func roll_state(delta):
+	#velocity = Vector2.ZERO
+	var input_vector = animationTree.get("parameters/Roll/blend_position")
+	velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	velocity = move_and_slide(velocity)
+		
+	animationState.travel("Roll")
+	
+func finish_roll_animation():
+	state = MOVE
+	
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+	
+func finish_attack_animation():
+	state = MOVE
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
