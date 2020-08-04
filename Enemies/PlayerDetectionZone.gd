60befe5dc8409716f3433dc2b33df958fe061b0a
Matt Stillwell <stillwell006@gmail.com>
2020-08-04 18:53:07 -0400

    bats follow

---
+++ Enemies/PlayerDetectionZone.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var player = null
+
+func can_see_player():
+	return player != null
+
+func _on_PlayerDetectionZone_body_entered(body):
+	player = body
+
+
+func _on_PlayerDetectionZone_body_shape_exited(body_id, body, body_shape, area_shape):
+	player = null
